7eb3748e03998c206d6f1b43bde15b81318f7237
sandrology <44675532+sandrology@users.noreply.github.com>
2025-04-10 17:25:48 +0200

    Update Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-primary text-white">
-      <div className="bg-primary container-custom py-12">
+      <div className="container-custom py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Brand */}
           <div>
